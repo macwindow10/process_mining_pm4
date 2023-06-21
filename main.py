676050cde430356817cfd27dd402e4825ee29871
mac.window.10 <mac.window.10@gmail.com>
2023-06-22 02:04:59 +0500

    main file added

--- main.py
+++ main.py
@@ -3,10 +3,15 @@ from sklearn.cluster import KMeans
 import pandas as pd
 from pm4py.objects.conversion.log import converter as log_converter
 from pm4py.algo.discovery.alpha import algorithm as alpha_miner
+from pm4py.algo.discovery.heuristics import algorithm as heuristics_miner
+from pm4py.algo.discovery.inductive import algorithm as inductive_miner
 from pm4py.algo.discovery.dfg import algorithm as dfg_discovery
 
 from pm4py.visualization.petri_net import visualizer as pn_visualizer
+from pm4py.visualization.heuristics_net import visualizer as hn_visualizer
 from pm4py.visualization.dfg import visualizer as dfg_visualization
+from pm4py.visualization.process_tree import visualizer as pt_visualizer
+
 from PIL import Image
 
 pd.set_option('display.max_columns', None)
@@ -56,3 +61,26 @@ dfg_visualization.view(gviz)
 dfg = dfg_discovery.apply(log, variant=dfg_discovery.Variants.PERFORMANCE)
 gviz = dfg_visualization.apply(dfg, log=log, variant=dfg_visualization.Variants.PERFORMANCE)
 dfg_visualization.view(gviz)
+
+# heuristics miner
+heu_net = heuristics_miner.apply_heu(log)
+gviz = hn_visualizer.apply(heu_net)
+hn_visualizer.view(gviz)
+hn_visualizer.save(gviz, 'heuristic_miner.png')
+
+# Petri-net of heuristic miner output
+net, im, fm = heuristics_miner.apply(log)
+gviz = pn_visualizer.apply(net, im, fm)
+gviz = pn_visualizer.apply(net, im, fm,
+                           parameters=parameters,
+                           variant=pn_visualizer.Variants.FREQUENCY,
+                           log=log)
+pn_visualizer.view(gviz)
+pn_visualizer.save(gviz, 'heuristic_miner_petri_net.png')
+
+# Inductive miner
+# create the process tree
+tree = inductive_miner.apply(log)
+gviz = pt_visualizer.apply(tree)
+pt_visualizer.view(gviz)
+pt_visualizer.save(gviz, 'inductive_miner.png')
