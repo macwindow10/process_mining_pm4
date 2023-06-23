c557359c3f4b35108d751c41dcd51194525cfab6
mac.window.10 <mac.window.10@gmail.com>
2023-06-23 22:57:55 +0500

    conference checking comparison graph

--- conformance_checking.py
+++ conformance_checking.py
@@ -25,7 +25,7 @@ log = log_converter.apply(df_selected)
 
 
 net, initial_marking, final_marking = pm4py.discover_petri_net_inductive(log)
-replayed_traces = pm4py.conformance_diagnostics_token_based_replay(log, net, initial_marking, final_marking)
+# replayed_traces = pm4py.conformance_diagnostics_token_based_replay(log, net, initial_marking, final_marking)
 # print(replayed_traces)
 fitness_inductive = pm4py.fitness_token_based_replay(log, net, initial_marking, final_marking)
 print(fitness_inductive)
@@ -44,30 +44,40 @@ print(fitness_heuristics)
 
 y1 = [
     fitness_inductive['average_trace_fitness'],
-    fitness_inductive['log_fitness']]
-y2 = [
     fitness_alpha['average_trace_fitness'],
-    fitness_alpha['log_fitness']]
-y3 = [
-    fitness_heuristics['average_trace_fitness'],
-    fitness_heuristics['log_fitness']]
+    fitness_heuristics['average_trace_fitness']
+]
 barWidth = 0.30
 br1 = np.arange(len(y1))
-br2 = [x + barWidth for x in br1]
-br3 = [x + barWidth for x in br2]
-
 fig = plt.subplots(figsize=(12, 8))
-
 # keys_fitness_inductive = list(fitness_inductive.keys())
 # values_fitness_inductive = list(fitness_inductive.values())
 plt.bar(br1, y1, color='maroon', width=barWidth)
-plt.bar(br2, y2, color='green', width=barWidth)
-plt.bar(br3, y3, color='blue', width=barWidth)
-
 plt.xlabel("Process Discovery Models")
-plt.ylabel("Conformance Matrics")
+plt.ylabel("Conformance Checking - Average Trace Fitness")
 plt.title("Comparison of Process Discovery Models")
-plt.xticks([r + barWidth for r in range(len(y1))],
-           ['Average Trace Fitness', 'Log Fitness'])
-plt.legend()
+plt.xticks([r for r in range(len(y1))],
+           ['Inductive Miner', 'Alpha Miner', 'Heuristic Miner'])
+xlocs, xlabs = plt.xticks()
+for i, v in enumerate(y1):
+    plt.text(xlocs[i] - 0.05, v + 0.01, str(round(v, 2)))
+plt.show()
+
+y1 = [
+    fitness_inductive['log_fitness'],
+    fitness_alpha['log_fitness'],
+    fitness_heuristics['log_fitness']
+]
+barWidth = 0.30
+br1 = np.arange(len(y1))
+fig = plt.subplots(figsize=(12, 8))
+plt.bar(br1, y1, color='blue', width=barWidth)
+plt.xlabel("Process Discovery Models")
+plt.ylabel("Conformance Checking - Log Fitness")
+plt.title("Comparison of Process Discovery Models")
+plt.xticks([r for r in range(len(y1))],
+           ['Inductive Miner', 'Alpha Miner', 'Heuristic Miner'])
+xlocs, xlabs = plt.xticks()
+for i, v in enumerate(y1):
+    plt.text(xlocs[i] - 0.05, v + 0.01, str(round(v, 2)))
 plt.show()
