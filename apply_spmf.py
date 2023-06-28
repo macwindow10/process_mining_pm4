f9998fc352723ba7f864dd3c3aca73327691be0c
mac.window.10 <mac.window.10@gmail.com>
2023-06-28 14:52:15 +0500

    iterate dataset over user or Case_id, get his all activities time-wise

--- apply_spmf.py
+++ apply_spmf.py
@@ -5,21 +5,31 @@ import matplotlib.pyplot as plt
 
 pd.set_option('display.max_columns', None)
 pd.set_option('display.max_rows', None)
+pd.set_option('expand_frame_repr', False)
 
 # Load the CSV file into a pandas DataFrame
 df = pd.read_csv('total_students.csv')
 df['timestamp'] = pd.to_datetime(df['timestamp'])
 timestamp = df.timestamp.unique()
 userids = df.Case_id.unique()
-print(timestamp)
-print(df.timestamp.min())
-print(df.timestamp.max())
-print(len(timestamp))
+# print(timestamp)
+# print(df.timestamp.min())
+# print(df.timestamp.max())
+# print(len(timestamp))
 # print(userids)
 # print(len(userids))
 # df.groupby(df["timestamp"].dt.day).count().plot(kind="bar")
-df.groupby([df["timestamp"].dt.month, df["timestamp"].dt.day]).count().plot(kind="bar")
-plt.show()
+# df.groupby([df["timestamp"].dt.month, df["timestamp"].dt.day]).count().plot(kind="bar")
+# plt.show()
+# for u in userids:
+list = [223]
+# print(u)
+print(list)
+# df.loc[df['ID'].isin(list_ID)]
+mask = df['Case_id'].isin(list)
+df_u = df.loc[mask]
+print(len(df_u))
+print(df_u.sort_values(by=['timestamp']))
 
 # Different input formats (apart from file):
 input_example_list = [
