2a1bdc124aded10e2f9eca4a62c59e1809bba9ad
mac.window.10 <mac.window.10@gmail.com>
2023-06-28 21:47:02 +0500

    GSP algorithm called for sequnces

--- apply_spmf.py
+++ apply_spmf.py
@@ -39,65 +39,21 @@ input_example_list = [
     [[5], [7], [1, 6], [3], [2], [3]]
 ]
 
-input_example_list_text = [
-    "w1 w1 w2 w3 w1 w3 w4 w3 w6",
-    "w1 w4 w3 w2 w3 w1 w5",
-    "w5 w6 w1 w2 w4 w6 w3 w2",
-    "w5 w7 w1 w6 w3 w2 w3"
-]
-
-input_example_raw = """1 -1 1 2 3 -1 1 3 -1 4 -1 3 6 -1 -2
-1 4 -1 3 -1 2 3 -1 1 5 -1 -2
-5 6 -1 1 2 -1 4 6 -1 3 -1 2 -1 -2
-5 -1 7 -1 1 6 -1 3 -1 2 -1 3 -1 -2
-"""
-
-input_example_raw_text = """w1 w1 w2 w3 w1 w3 w4 w3 w6.
-w1 w4 w3 w2 w3 w1 w5.
-w5 w6 w1 w2 w4 w6 w3 w2.
-w5 w7 w1 w6 w3 w2 w3.
-"""
-
 """
 # Different algorithms:
-spmf = Spmf("PrefixSpan", input_filename="contextPrefixSpan.txt",
+spmf = Spmf("GSP", input_filename="contextPrefixSpan.txt",
             output_filename="output.txt",
             spmf_bin_location_dir="venv/Lib/site-packages/spmf/",
-            arguments=[0.7, 5])
-
-# spmf = Spmf("SPAM", input_filename="contextPrefixSpan.txt",
-# output_filename="output.txt", arguments=[0.6, "", 5])
-"""
-
-# Different arguments:
-
-# spmf = Spmf("PrefixSpan", input_filename="contextPrefixSpan.txt",
-# output_filename="output.txt", arguments=[1, "", True])
-
-# spmf = Spmf("PrefixSpan", input_direct=input_example_raw,
-# output_filename="output.txt",
-# arguments=[1, "", True])
-
-# spmf = Spmf("PrefixSpan", input_direct=input_example_raw_text,
-# input_type="text",
-# output_filename="output.txt",
-# arguments=[1, "", True])
-
-# spmf = Spmf("PrefixSpan", input_direct=input_example_list,
-# output_filename="output.txt",
-# arguments=[1, "", True])
-
-# spmf = Spmf("PrefixSpan", input_direct=input_example_list_text,
-# input_type="text",
-# output_filename="output.txt",
-# arguments=[1, "", True])
-
-# spmf.run()
+            arguments=[0.7, 1])
+spmf.run()
 # print(spmf.parse_output())
 # print(spmf.to_pandas_dataframe(pickle=True))
-# spmf.to_csv("output.csv")
+spmf.to_csv("output.csv")
+"""
 
-# print('reading pkl file')
-with open('output.pkl', 'rb') as f:
-    data = pickle.load(f)
-    # print(data)
+spmf = Spmf("GSP", input_filename="contextGSP.txt",
+            output_filename="output_gsp.txt",
+            spmf_bin_location_dir="venv/Lib/site-packages/spmf/",
+            arguments=[0.7, 1])
+spmf.run()
+spmf.to_csv("output_gsp.csv")
