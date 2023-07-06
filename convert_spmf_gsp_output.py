ee534bbb1d4bad745c75caa9b9058223f5b6348a
mac.window.10 <mac.window.10@gmail.com>
2023-07-06 23:55:24 +0500

    SMPF GSP output converted to designated format

--- convert_spmf_gsp_output.py
+++ convert_spmf_gsp_output.py
@@ -23,30 +23,49 @@ def mapping_of(v):
     if v == 'user_logout':
         return 'j'
 
+
 def main():
     file_output_spmfgsp = open("output_spmfgsp.txt", "r")
     lines = file_output_spmfgsp.readlines()
     count = 0
     outputs = ""
+    add_support_value = False
+    add_line_number = False
     for line in lines:
         count += 1
         line_values = line.split('|')
         output = ""
         for line_value in line_values:
             if line_value.strip().startswith("#SUP:"):
-                print('ignore #SUP:')
+                support_line = line_value.strip()
+                # print('Support and SIDs: ', support_line)
+                support = support_line.split(":")[1].strip().split(' ')[0].strip()
+                # print(support)
             else:
                 line_value = line_value.strip()
                 line_value_items = line_value.split(' ')
+                if len(output) > 0:
+                    output = output + ", "
                 output = output + "<("
                 if len(line_value_items) == 1:
                     v = mapping_of(line_value_items[0])
-                    print(v, ',', line_value_items)
-                    if v is NoneType:
-                        print(line_value_items[0])
+                    # print(v, ',', line_value_items)
                     output = output + v
                     output = output + ")>"
-        outputs = outputs + output + '\n'
+                else:
+                    for line_value_item in line_value_items:
+                        # print(line_value_item)
+                        v = mapping_of(line_value_item)
+                        output = output + v + ", "
+                    output = output.rstrip(' ').rstrip(',')
+                    output = output + ")>"
+        if add_line_number:
+            outputs = outputs + str(count) + ": " + output + '\n'
+        else:
+            if add_support_value:
+                outputs = outputs + output + '\t\t' + support + '\n'
+            else:
+                outputs = outputs + output + '\n'
     file_output_spmfgsp.close()
     print(outputs)
 
